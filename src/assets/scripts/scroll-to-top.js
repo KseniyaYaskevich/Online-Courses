6dbe7869a9d44ee9800a3b880d7885480b273fdd
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-01-24 15:00:23 +0300

    feat: add scroll to top

---
+++ src/assets/scripts/scroll-to-top.js
@@ -0,0 +1,10 @@
+const toTopBtn = document.querySelector('#button-to-top');
+
+const scrollToTop = () => {
+  window.scrollBy({
+    top: -document.documentElement.scrollHeight,
+    behavior: 'smooth'
+  });
+};
+
+toTopBtn.addEventListener('click', scrollToTop);
