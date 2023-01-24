0834fa52b1eef3593262fc61c87dbb35f4301c6e
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-01-24 14:31:31 +0300

    fix: rename scripts according to their functions

---
+++ src/assets/scripts/animate-form-labels.js
@@ -0,0 +1,12 @@
+const labels = document.querySelectorAll('#form-label');
+
+const animateFormLabels = () => {
+  labels.forEach((label) => {
+    label.innerHTML = label.innerText
+      .split('')
+      .map((letter, idx) => `<span style="transition-delay:${idx * 30}ms">${letter}</span>`)
+      .join('');
+  });
+};
+
+animateFormLabels();
