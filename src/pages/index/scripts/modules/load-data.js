ddd269cc9d328180873eb89d649e0f7c340f0de2
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-01-30 14:57:47 +0300

    feat: add load-data.js

---
+++ src/pages/index/scripts/modules/load-data.js
@@ -0,0 +1,16 @@
+import {getData} from 'scripts/modules/get-course-data';
+import {getItems} from 'scripts/modules/utils';
+import {render} from 'scripts/modules/utils';
+import {createCourseTemplate} from 'scripts/view/course-template';
+
+const cardsWrapper = document.querySelector('.courses__list');
+
+export const loadData = (cardsNumber) => {
+  getData().then((data) => {
+    const cards = getItems(data, cardsNumber);
+
+    cards.forEach((item) => {
+      render(cardsWrapper, createCourseTemplate(item, 'horizontal'));
+    });
+  });
+};
