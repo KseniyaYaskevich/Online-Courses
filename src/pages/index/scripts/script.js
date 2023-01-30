37964ca03ebc2d65c3ceb0fec4f322d3c8c8872e
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-01-30 14:59:23 +0300

    feat: call loadData function with parameters

--- src/pages/index/scripts/script.js
+++ src/pages/index/scripts/script.js
@@ -3,3 +3,9 @@ import 'scripts/modules/swiper-testimonials';
 
 import './modules/benefits-tabs';
 import './modules/swiper-team';
+
+import {loadData} from './modules/load-data';
+
+const CARDS_NUMBER = 6;
+
+loadData(CARDS_NUMBER);
