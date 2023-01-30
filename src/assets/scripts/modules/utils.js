ba16a70acc81fd08e6687aeb321fb44e34df4e5a
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-01-30 14:55:32 +0300

    feat: add utils.js

---
+++ src/assets/scripts/modules/utils.js
@@ -0,0 +1,16 @@
+const getRandomNumber = (min = 1, max) => Math.random() * (max - min) + min;
+
+export const getItems = (data, number) => {
+  const newData = data;
+  const tempArray = [];
+
+  for (let i = 0; i < number; i++) {
+    const randomIndex = Math.floor(getRandomNumber(1, 9));
+    tempArray.push(newData[randomIndex]);
+  }
+  return tempArray;
+};
+
+export const render = (container, template) => {
+  container.append(template);
+};
