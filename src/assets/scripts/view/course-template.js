90bacf6c865fb291baad2d86089e0dd3538d7289
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-02-13 15:07:31 +0300

    fix: way to create an element

--- src/assets/scripts/view/course-template.js
+++ src/assets/scripts/view/course-template.js
@@ -5,20 +5,15 @@ export const createCourseTemplate = ({
   title,
   price,
   curator
-}, view) => {
-  const cardElement = document.createElement('li');
-  cardElement.classList.add('courses__item');
-  cardElement.innerHTML = `
-          <a class="courses__link course-card course-card--${view}" href="course.html" target="_blank">
-              <img class="course-card__photo" src="${photo}"
-              alt="Photo of speaker Jerome Bell">
-              <div class="course-card__content">
-                  <p class="course-card__profession course-card__profession--badge-${type.toLowerCase()}">${specification}</p>
-                  <h3 class="course-card__title">${title}</h3>
-                  <span class="course-card__price">$${price}</span>
-                  <span class="course-card__author">| by ${curator}</span>
-              </div>
-          </a>
-      `;
-  return cardElement;
-};
+}, view) => `<li class="courses__item">
+            <a class="courses__link course-card course-card--${view}" href="course.html" target="_blank">
+                <img class="course-card__photo" src="${photo}"
+                alt="Photo of speaker Jerome Bell">
+                <div class="course-card__content">
+                    <p class="course-card__profession course-card__profession--badge-${type.toLowerCase()}">${specification}</p>
+                    <h3 class="course-card__title">${title}</h3>
+                    <span class="course-card__price">$${price}</span>
+                    <span class="course-card__author">| by ${curator}</span>
+                </div>
+            </a>
+          </li>`;
