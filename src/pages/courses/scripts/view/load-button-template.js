2d845279f7d74f58af0b395dd0573c6ec46f0dfe
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-02-13 15:09:04 +0300

    feat: add create load more button template

---
+++ src/pages/courses/scripts/view/load-button-template.js
@@ -0,0 +1,6 @@
+export const createLoadButtonTemplate = () => `<button class="button courses__button" id="load-more" type="button">
+                                                <svg class="courses__icon" width="24" height="24">
+                                                  <use xlink:href="assets/images/sprite.svg#convert"></use>
+                                                </svg>
+                                                Load more
+                                              </button>`;
