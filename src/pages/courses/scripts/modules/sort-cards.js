002279227b6e9f2933ba7d5cff9828275c642bf3
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-02-13 15:15:09 +0300

    feat: add sort-cards.js

---
+++ src/pages/courses/scripts/modules/sort-cards.js
@@ -0,0 +1,35 @@
+import {createCards} from './create-cards';
+
+const sortingButtons = document.querySelectorAll('.courses-sorting__button');
+
+export const sortCards = (array, cardsPerStep) => {
+  sortingButtons.forEach((button) => {
+    const type = button.id.toLowerCase();
+    const cardsByType = [];
+    let count = 0;
+
+    array.forEach((card) => {
+      const coursesCount = button.querySelector('.courses-sorting__count');
+
+      if (type === 'all') {
+        count = array.length;
+        cardsByType.push(card);
+      } else if (card.type.toLowerCase() === type) {
+        count++;
+        cardsByType.push(card);
+      }
+      coursesCount.innerHTML = `${count}`;
+    });
+
+    const onSortingButtonClick = () => {
+      sortingButtons.forEach((btn) => {
+        btn.classList.remove('courses-sorting__button--active');
+      });
+
+      button.classList.add('courses-sorting__button--active');
+      createCards(cardsByType, cardsPerStep);
+    };
+
+    button.addEventListener('click', onSortingButtonClick);
+  });
+};
