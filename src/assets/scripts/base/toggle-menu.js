7972faae6a071a0cf9ca595846283f15b154a856
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-02-14 17:15:58 +0300

    feat: add function to change tabIndex for links

--- src/assets/scripts/base/toggle-menu.js
+++ src/assets/scripts/base/toggle-menu.js
@@ -1,10 +1,20 @@
 const pageBody = document.querySelector('.page__body');
 const toggleButton = pageBody.querySelector('.menu__toggle');
 const navbarMenu = pageBody.querySelector('.navbar__menu');
+const navbarLinks = pageBody.querySelectorAll('.menu__link');
+
+const changeTabindexForLinks = () => {
+  if (navbarMenu.classList.contains('is-opened')) {
+    navbarLinks.forEach((link) => (link.attributes.tabIndex.value = '0'));
+  } else {
+    navbarLinks.forEach((link) => (link.attributes.tabIndex.value = '-1'));
+  }
+};
 
 const onToggleButtonClick = () => {
   pageBody.classList.toggle('page__body--lock');
   navbarMenu.classList.toggle('is-opened');
+  changeTabindexForLinks();
 };
 
 toggleButton.addEventListener('click', onToggleButtonClick);
