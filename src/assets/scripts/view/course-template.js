fa663c68003c7667cbd70545ab291456f75241bd
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-02-28 15:17:04 +0300

    fix: rework createCourseTemplate function

--- src/assets/scripts/view/course-template.js
+++ src/assets/scripts/view/course-template.js
@@ -1,19 +1,23 @@
 export const createCourseTemplate = ({
-  photo,
+  id,
   type,
   specification,
   title,
   price,
   curator
-}, view) => `<li class="courses__item">
-            <a class="courses__link course-card course-card--${view}" href="course.html" target="_blank">
-                <img class="course-card__photo" src="${photo}"
-                alt="Photo of speaker Jerome Bell">
-                <div class="course-card__content">
-                    <p class="course-card__profession course-card__profession--badge-${type.toLowerCase()}">${specification}</p>
-                    <h3 class="course-card__title">${title}</h3>
-                    <span class="course-card__price">$${price}</span>
-                    <span class="course-card__author">| by ${curator}</span>
-                </div>
-            </a>
-          </li>`;
+}, view, swiperSlide = '') => `
+<li class="courses__item${swiperSlide ? ` ${swiperSlide}` : ''}">
+  <a class="courses__link course-card course-card--${view}" href="course.html" data-id="${id}" target="_blank">
+    <picture>
+      <source srcset="./assets/images/speakers/speaker-${id}.webp" type="image/webp">
+      <img class="course-card__photo" src="./assets/images/speakers/speaker-${id}.jpg"
+      alt="Photo of speaker ${curator}">
+    </picture>
+    <div class="course-card__content">
+        <p class="course-card__profession course-card__profession--badge-${type.toLowerCase()}">${specification}</p>
+        <h3 class="course-card__title">${title}</h3>
+        <span class="course-card__price">$${price}</span>
+        <span class="course-card__author">| by ${curator}</span>
+    </div>
+  </a>
+</li>`;
