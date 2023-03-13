7709beb4722b73d4c484804c04f7f5409978d62b
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-13 15:04:36 +0300

    fix: import and call to getData function

--- src/pages/courses/scripts/modules/load-data.js
+++ src/pages/courses/scripts/modules/load-data.js
@@ -1,11 +1,11 @@
-import {getData} from 'scripts/modules/get-course-data';
+import {getData} from 'scripts/modules/get-data';
 import {getItems} from 'scripts/modules/utils';
 import {createCards} from './create-cards';
 import {sortCards} from './sort-cards';
 
 export const loadData = (cardsNumber, cardsPerStep) => {
 
-  getData().then((data) => {
+  getData('courses').then((data) => {
     const cards = getItems(data, cardsNumber);
 
     createCards(cards, cardsPerStep);
--- src/pages/index/scripts/modules/load-data.js
+++ src/pages/index/scripts/modules/load-data.js
@@ -1,4 +1,4 @@
-import {getData} from 'scripts/modules/get-course-data';
+import {getData} from 'scripts/modules/get-data';
 import {getItems} from 'scripts/modules/utils';
 import {render} from 'scripts/modules/utils';
 import {createCourseTemplate} from 'scripts/view/course-template';
@@ -6,7 +6,7 @@ import {createCourseTemplate} from 'scripts/view/course-template';
 const cardsWrapper = document.querySelector('.courses__list');
 
 export const loadData = (cardsNumber) => {
-  getData().then((data) => {
+  getData('courses').then((data) => {
     const cards = getItems(data, cardsNumber);
 
     cards.forEach((item) => {
