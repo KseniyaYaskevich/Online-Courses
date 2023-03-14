a98a574f073a5faff7d4806df269201899cf42e7
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-14 16:25:43 +0300

    feat: add imports

---
+++ src/pages/events/scripts/script.js
@@ -0,0 +1,6 @@
+import 'scripts/script';
+
+import './modules/swiper-events';
+
+import {loadData} from './modules/load-data';
+import {eventCardsHandler} from 'scripts/modules/event-cards-handler';
