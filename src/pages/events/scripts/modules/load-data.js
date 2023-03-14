02f687c9f35d79dd4bdc272accd9cc28d95da21e
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-14 16:24:07 +0300

    feat: add load-data.js

---
+++ src/pages/events/scripts/modules/load-data.js
@@ -0,0 +1,16 @@
+import {getData} from 'scripts/modules/get-data';
+import {getItems} from 'scripts/modules/utils';
+import {render} from 'scripts/modules/utils';
+import {createEventTemplate} from '../view/event-template';
+
+const cardsWrapper = document.querySelector('.events__list');
+
+export const loadData = (cardsNumber) => {
+  getData('events').then((data) => {
+    const cards = getItems(data, cardsNumber);
+
+    cards.forEach((item) => {
+      render(cardsWrapper, createEventTemplate(item));
+    });
+  });
+};
