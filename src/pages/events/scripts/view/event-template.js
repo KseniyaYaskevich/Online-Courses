b4261ff5b12ef1ac67ac571de11e853c0e2c1eab
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-14 16:26:18 +0300

    feat: add event-template.js

---
+++ src/pages/events/scripts/view/event-template.js
@@ -0,0 +1,22 @@
+export const createEventTemplate = ({
+  id,
+  day,
+  month,
+  time,
+  title,
+  type
+}) => `
+<li class="events__item event-card swiper-slide" data-id="${id}">
+  <div class="event-card__data">
+    <p class="event-card__day">${day}</p>
+    <p class="event-card__month">${month}</p>
+    <time class="event-card__time">${time}</time>
+  </div>
+  <p class="event-card__info">
+    ${title}
+    <span class="event-card__desc">${type}</span>
+  </p>
+  <a class="event-card__button button button--size-regular button--color-transparent" href="event.html" target="_blank">
+    View more
+  </a>
+</li>`;
