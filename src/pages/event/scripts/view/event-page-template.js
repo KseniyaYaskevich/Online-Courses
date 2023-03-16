be10ea25d6d805f70f6515a5e69119cd0ee91e1c
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-16 13:21:54 +0300

    feat: add event-page-template.js

---
+++ src/pages/event/scripts/view/event-page-template.js
@@ -0,0 +1,16 @@
+import {render} from 'scripts/modules/utils';
+import {createEventListTemplate} from './event-list-template';
+import {createCuratorTemplate} from './curator-template';
+
+export const createEventPageTemplate = (item) => {
+  const pageTitle = document.querySelector('.page-header__title');
+  const pageSubtitle = document.querySelector('.page-header__subtitle');
+  const eventList = document.querySelector('.about__event');
+  const curatorContainer = document.querySelector('.curator .container');
+
+  pageTitle.innerHTML = item.title;
+  pageSubtitle.innerHTML = item.type;
+
+  render(eventList, createEventListTemplate(item));
+  render(curatorContainer, createCuratorTemplate(item));
+};
