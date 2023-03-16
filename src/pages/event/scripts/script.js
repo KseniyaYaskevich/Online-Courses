8770216941d814f87623cc27553b7c90cea5b144
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-16 13:23:09 +0300

    feat: add window loading handler

--- src/pages/event/scripts/script.js
+++ src/pages/event/scripts/script.js
@@ -4,3 +4,6 @@ import 'scripts/modules/spollers';
 import {createEventPageTemplate} from './view/event-page-template';
 import {getItem} from 'scripts/base/storage';
 
+window.addEventListener('load', () => {
+  createEventPageTemplate(getItem('event'));
+});
