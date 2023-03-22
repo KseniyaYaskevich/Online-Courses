945a1e7adc649640d2d292ea8ba27b2aca60e6db
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-03-22 12:13:14 +0300

    feat: add load-data.js

---
+++ src/pages/blog/scripts/modules/load-data.js
@@ -0,0 +1,17 @@
+import {getData} from 'scripts/modules/get-data';
+import {getItems} from 'scripts/modules/utils';
+import {render} from 'scripts/modules/utils';
+
+import {createBlogItemTemplate} from 'scripts/view/blog-item-template';
+import {sortCards} from './sort-cards';
+
+const cardsWrapper = document.querySelector('.blog__list');
+
+export const loadData = (cardsNumber) => {
+  getData('blog').then((data) => {
+    const cards = getItems(data, cardsNumber);
+    cards.forEach((card) =>
+      render(cardsWrapper, createBlogItemTemplate(card, 'swiper-slide')));
+    sortCards(cards);
+  });
+};
