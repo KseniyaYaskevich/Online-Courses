48d40b61ddc942f12681715f3d9a4c9fa8209feb
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2023-04-03 17:13:15 +0300

    fix: constant

--- src/pages/blog/scripts/script.js
+++ src/pages/blog/scripts/script.js
@@ -5,7 +5,7 @@ import './modules/swiper-blog';
 import {loadData} from './modules/load-data';
 import {blogCardsHandler} from 'scripts/modules/blog-cards-handler';
 
-const CARDS_NUMBER = 36;
+const CARDS_NUMBER = 66;
 
 loadData(CARDS_NUMBER);
 blogCardsHandler();
